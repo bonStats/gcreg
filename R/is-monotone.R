03c2aed53c8c95aad6fd6a1586abf88204bfa10c
Joshua Bon <joshuajbon@gmail.com>
2017-08-16 12:39:54 +1000

    Removed is-monotone type diagnostic.

--- R/is-monotone.R
+++ R/is-monotone.R
@@ -41,52 +41,13 @@ is_monotone <- function(p, region = c(-Inf,Inf), type = NULL, EPS = 1e-05){
   crit_roots <- roots_re[abs(roots_im) < EPS & a + EPS < roots_re & roots_re < b - EPS]
   
   # check multiplicity of critical roots
-  crit_roots_mltplcty <- rowSums( outer(crit_roots, crit_roots, function(x, y) abs(x - y) < EPS) )
+  crit_roots_mltp <- rowSums( outer(crit_roots, crit_roots, function(x, y) abs(x - y) < EPS) )
   
-  # test for derivative roots
-  if(any(crit_roots_mltplcty %% 2 != 0)){
-    mono <- FALSE
-    attr(mono,"region") <- region
-    attr(mono,"type") <- "monotone increasing or decreasing"
-    return(mono)
-  }
+  # test for derivative roots having multiplicity 2
   
-  # inner points can be checked for monotone increasing or decreasing
-  finite_reg <- region[is.finite(region)]
+  mono <- all(crit_roots_mltp %% 2 == 0)
+  attr(mono,"region") <- region
   
-  if(length(finite_reg) == 2){
-    
-    inner_point <- mean(finite_reg)
-    
-  } else if(length(finite_reg) == 1) {
-    
-    inner_point <- ifelse(finite_reg == a, a + 1, b - 1)
-    
-  } else {
-    
-    inner_point <- 0
-    
-  }
-
-  crit_value <- predict(D_pl,inner_point)
-  
-  # should i remove EPS 
-  
-  if(crit_value > 0){
-    mono <- ifelse(usetype, "increasing" == type, TRUE)
-    attr(mono,"region") <- region
-    attr(mono,"type") <- "monotone increasing"
-    return(mono)
-  } else if(crit_value < 0){
-    mono <- ifelse(usetype, "decreasing" == type, TRUE)
-    attr(mono,"region") <- region
-    attr(mono,"type") <- "monotone decreasing"
-    return(mono)
-  } else {
-    mono <- TRUE
-    attr(mono,"region") <- region
-    attr(mono,"type") <- "unsure, check different critical value"
-    return(mono)
-  }
+  return(mono)
 
 }
