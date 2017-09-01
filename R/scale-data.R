97bf48af59c1825279af9926c0734917fe6700d4
Joshua Bon <joshuajbon@gmail.com>
2017-09-01 10:25:22 +1000

    Exporting scale data function, added attributes for unscaling

--- R/scale-data.R
+++ R/scale-data.R
@@ -1,13 +1,27 @@
-gen_scale_data_funcs <- function(x){
+#' Create function to scale and unscale data.
+#' 
+#' Scale x variables to be between -1 and 1 which improves stability of algorithms working with polynomials
+#' @param x vector of data.
+#' @export
+#' @examples
+#' x <- rnorm(100, mean = 3)
+#' sc <- gen_scale_data_funs(x)
+#' x_scaled <- sc$scale(x)
+#' all(abs(sc$unscale(x_scaled) - x) < 1e-10)
+#' 
+gen_scale_data_funs <- function(x){
   #scale to min = -1, max = 1
   xmax <- max(x) 
   b <- 2/(xmax - min(x))
   c <- 1 - b * xmax
   
-  return(
-    list(scale   = function(x){ b * x + c },
-         unscale = function(x){ (x - c)/b }
-    )
+  fs <- list(scale   = function(x){ b * x + c },
+             unscale = function(x){ (x - c)/b }
   )
+  
+  attr(fs, "b") <- b
+  attr(fs, "c") <- c
+  
+  return(fs)
   
 }
