ccd5ffec70670e7999d7d8fce81144afb7133667
Joshua Bon <joshuajbon@gmail.com>
2017-10-04 14:03:26 +1000

    Added polynomial basis to design matrix function.

---
+++ R/poly-basis-design-matrix.R
@@ -0,0 +1,13 @@
+#' Create design matrix from polynomial basis.
+#' 
+#' @param poly_basis polylist defining the (non-monomial) basis to convert from.
+#' @return Design matrix for polynomial regression in \code{ploy_basis}
+#' @export
+
+poly_basis_design_matrix <- function(poly_basis, x){
+  
+  if(!polynom::is.polylist(poly_basis)) warning("poly_basis should be polylist object")
+  
+  sapply(poly_basis, predict, newdata = x)
+  
+}
