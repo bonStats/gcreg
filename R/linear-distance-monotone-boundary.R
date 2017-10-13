e2d3c13bb4b41c8dd138fe64777ac6d1f6d30e88
Joshua Bon <joshuajbon@gmail.com>
2017-10-13 19:15:02 +1000

    Fixed warning message for infinite boundaries.

--- R/linear-distance-monotone-boundary.R
+++ R/linear-distance-monotone-boundary.R
@@ -16,11 +16,19 @@ linear_dist_to_monotone_boundary <- function(gam, region, basis_cv, EPS = 1e-06)
   bet <- polynomial(basis_cv$to_mono(gam))
   
   reg <- sort(region)
+  reset_reg <- F
   
   #replace infinite regions with large testable number
-  if(is.infinite(reg[1])) reg[1] <- -100
-  if(is.infinite(reg[2])) reg[2] <- 100
-    
+  if(is.infinite(reg[1])){
+    reg[1] <- -100
+    reset_reg <- T
+  }
+  
+  if(is.infinite(reg[2])){
+    reg[2] <- 100
+    reset_reg <- T
+  } 
+  
   # This effects the optimisation for aim_gamma
   
   #calculate first, second derivative + roots
@@ -42,7 +50,7 @@ linear_dist_to_monotone_boundary <- function(gam, region, basis_cv, EPS = 1e-06)
   which_min_Dpl <- which.min(DDpl_crit_vals)
   location_x <- crit_v[which_min_Dpl]
   
-  if(which_min_Dpl %in% c(1,2)) warning("linear distance to the monotone boundary based on constrained region = [", paste(reg, collapse = ","), "] instead of [", paste(region, collapse = ","), "]")
+  if(reset_reg & which_min_Dpl %in% c(1,2)) warning("linear distance to the monotone boundary based on constrained region = [", paste(reg, collapse = ","), "] instead of [", paste(region, collapse = ","), "]")
   
   # in monomial basis
   linear_dist <- DDpl_crit_vals[which_min_Dpl]
